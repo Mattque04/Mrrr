82425dbd3713a5943abfb0c1f995e99ea9c46564
SRdeagle <aki.jeremic@gmail.com>
2023-01-19 00:05:00 +0100

    adasd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,7 +30,7 @@
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">MRRRRRRRRRRRRR</a>
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">laku noc</a>
                         @endif
                     @endauth
                 </div>
